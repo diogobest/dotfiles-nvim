45acce8bb7cc4bb1420509c2b5fca4c8a40584c0
Diogo <dev-02@sscrop.com>
2026-05-12 14:27:46 -0300

    fix endwise

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,35 @@
 return {
   -- { "ellisonleao/gruvbox.nvim" },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  -- },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     -- colorscheme = "gruvbox",
+  --     colorscheme = "tokyonight-night",
+  --   },
+  -- },
   {
-    "folke/tokyonight.nvim",
+    "vague-theme/vague.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
     lazy = false,
     priority = 1000,
-    opts = {},
+    -- you can set set configuration options here
+    -- config = function()
+    --     vim.g.zenbones_darken_comments = 45
+    --     vim.cmd.colorscheme('zenbones')
+    -- end
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      -- colorscheme = "gruvbox",
-      colorscheme = "tokyonight-night",
+      colorscheme = "vague",
     },
   },
 }
